6b334708f8b0e047635b48bc4cf3c3e7eed73dfb
Xuan Huong <lxhuong484@gmail.com>
2022-01-03 12:21:27 +0700

    upload personal_yourself

---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+const iconDarkLight = document.querySelector('.app__dark-mode-icon--wrapper')
+const appTheme = document.querySelector('.app')
+
+appTheme.classList.add('light')
+
+iconDarkLight.onclick = (e) => {
+    // change theme
+    if (appTheme.classList.contains('light')) {
+        console.log("remove light")
+        appTheme.classList.remove('light')
+        appTheme.classList.add('dark')
+    } else {
+        console.log("remove dark")
+        appTheme.classList.add('light')
+        appTheme.classList.remove('dark')
+    }
+
+    // change icon 
+    const iconNode = e.target.closest('.app__dark-mode-icon')
+    if (e.target.closest('.bx-sun')) {
+        iconNode.classList.remove('bx-sun')
+        iconNode.classList.add('bx-moon')
+    } else if (e.target.closest('.bx-moon')) {
+        iconNode.classList.add('bx-sun')
+        iconNode.classList.remove('bx-moon')
+    }
+}
